a3989f4f57250afab2a0472e83b4482b01d776c0
wayne <sumel014@naver.com>
2017-06-05 20:16:10 +0900

    추가

--- class/User.js
+++ class/User.js
@@ -1,19 +1,21 @@
- class User {
-   constructor(id, ps){
-     this.id = id;
-     this.ps = ps;
-   }
-  
-   getUserInfo(){
-     return {
-       id : this.id,
-       ps : this.ps
-     }
-   }
- }
- 
- let user = new User("wayne", 1234);
- console.log(user.getUserInfo());
- 
- console.log(User instanceof Object);
- console.log(User instanceof Function);
+class User {
+  constructor(id, ps) {
+    this.id = id;
+    this.ps = ps;
+  }
+
+  getUserInfo() {
+    return {
+      id: this.id,
+      ps: this.ps
+    }
+  }
+}
+
+let user = new User("wayne", 1234);
+console.log(user.getUserInfo());
+
+console.log(User instanceof Object);
+console.log(User instanceof Function);
+
+//정신 없구만
